c6ec549f236a1cc03b9548b9aa2a536d659c605b
Matt Ewell <matt.ewell@gmail.com>
2023-02-20 14:36:50 -0700

    Add initial solution

--- pangram/Pangram.cs
+++ pangram/Pangram.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Pangram
 {
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+    
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        var foundLetters = input
+            .ToCharArray()
+            .ToList()
+            .Aggregate(new HashSet<char>(), (letters, letter) =>
+        {
+            var lowerChar = letter.ToString().ToLowerInvariant();
+
+            if(Alphabet.Contains(lowerChar))
+            {
+                letters.Add(lowerChar.ToCharArray()[0]);
+            }
+
+            return letters;
+        })
+            .Order();
+
+        return string.Join("", foundLetters) == Alphabet;
     }
 }
