4c7f0f98598dbb1a2264688adc9024e8528f344a
Matt Ewell <matt.ewell@gmail.com>
2023-04-04 16:17:49 -0600

    Add initial proverb solution

--- proverb/Proverb.cs
+++ proverb/Proverb.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 
 public static class Proverb
 {
-    public static string[] Recite(string[] subjects)
-    {
-        throw new NotImplementedException();
-    }
+    public static string[] Recite(string[] subjects) =>
+        subjects.Select((s, i) => i == subjects.Length - 1
+            ? $"And all for the want of a {subjects[0]}."
+            : $"For want of a {s} the {subjects[i + 1]} was lost.").ToArray();
 }
