1cacca45a9237a385d59573c9cc870fd78b36320
Matt Ewell <matt.ewell@gmail.com>
2023-04-04 16:21:59 -0600

    Switch order

--- proverb/Proverb.cs
+++ proverb/Proverb.cs
@@ -4,7 +4,8 @@ using System.Linq;
 public static class Proverb
 {
     public static string[] Recite(string[] subjects) =>
-        subjects.Select((s, i) => i == subjects.Length - 1
-            ? $"And all for the want of a {subjects[0]}."
-            : $"For want of a {s} the {subjects[i + 1]} was lost.").ToArray();
+        subjects.Select((s, i) => i < subjects.Length - 1
+            ? $"For want of a {s} the {subjects[i + 1]} was lost."
+            : $"And all for the want of a {subjects[0]}.")
+        .ToArray();
 }
