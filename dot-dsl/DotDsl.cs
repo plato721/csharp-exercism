b07f47c8c0cf15efbf69bd2fee8cdc02e2afc98b
Matt Ewell <matt.ewell@gmail.com>
2026-05-16 07:11:07 -0600

    Dot DSL - tighten up

--- dot-dsl/DotDsl.cs
+++ dot-dsl/DotDsl.cs
@@ -2,49 +2,35 @@ using System.Collections;
 
 using Xunit.Runner.Common;
 
-public class Node : IEquatable<Node>, IEnumerable
+public class Graph : Component
+{
+    public List<Node> Nodes { get; } = [];
+    public List<Edge> Edges { get; } = [];
+    public void Add(Node node) => Nodes.Add(node);
+    public void Add(Edge edge) => Edges.Add(edge);
+}
+
+public class Component : IEnumerable
+{
+    public List<Attr>? Attrs { get; } = [];
+    public void Add(string label, string value) => Attrs.Add(new Attr(label, value));
+    public IEnumerator GetEnumerator() => Attrs.GetEnumerator();
+}
+
+public class Node : Component
 {
     public readonly string? Name;
     public Dictionary<string, string> Values { get; } = [];
-    public List<Attr>? Attrs { get; } = [];
-
-
-    public Node(string? name)
-    {
-        Name = name;
-    }
-
-    public void Add(string label, string value)
-    {
-        Attrs.Add(new Attr(label, value));
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return Values.GetEnumerator();
-    }
-
-    public bool Equals(Node? other)
-    {
-        return Name == other?.Name;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return Equals(obj as Node);
-    }
-
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Name);
-    }
+    public Node(string? name) => Name = name;
+    public bool Equals(Node? other) => Name == other?.Name;
+    public override bool Equals(object? obj) => Equals(obj as Node);
+    public override int GetHashCode() => HashCode.Combine(Name);
 }
 
-public class Edge : IEnumerable
+public class Edge : Component
 {
     public readonly string NodeName1;
     public readonly string NodeName2;
-    public List<Attr>? Attrs { get; } = [];
 
     public Edge(string nodeName1, string nodeName2)
     {
@@ -52,57 +38,14 @@ public class Edge : IEnumerable
         NodeName2 = nodeName2;
     }
 
-    public void Add(string label, string value)
-    {
-        Attrs.Add(new Attr(label, value));
-    }
-
-    public IEnumerator GetEnumerator()
-    {
-        yield break;
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return Equals(obj as Edge);
-    }
+    public override bool Equals(object? obj) => Equals(obj as Edge);
 
     public bool Equals(Edge? other)
     {
         return other.NodeName1 == NodeName1 && other.NodeName2 == NodeName2;
     }
 
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(NodeName1, NodeName2);
-    }
+    public override int GetHashCode() => HashCode.Combine(NodeName1, NodeName2);
 }
 
 public record Attr(string Key, string Value);
-
-public class Graph : IEnumerable
-{
-    public List<Node> Nodes { get; } = [];
-    public List<Edge>? Edges { get; } = [];
-    public List<Attr>? Attrs { get; } = [];
-
-    public void Add(Node node)
-    {
-        Nodes.Add(node);
-    }
-
-    public void Add(Edge edge)
-    {
-        Edges.Add(edge);
-    }
-
-    public void Add(string label, string value)
-    {
-        Attrs.Add(new(label, value));
-    }
-
-    public IEnumerator GetEnumerator()
-    {
-        yield break;
-    }
-}
